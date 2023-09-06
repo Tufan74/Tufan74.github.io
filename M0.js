46a0d007035d4fd2cf4af8cfc1a8750b165fc1d7
Tufan <81257619+Tufan74@users.noreply.github.com>
2023-09-06 23:36:48 +0200

    h

--- M0.js
+++ M0.js
@@ -1,10 +1,5 @@
 <script>
-fetch('/get-ip.php')
-            .then(response => response.text())
-            .then(ip => {
-                document.getElementById('ip-address').textContent = ip;
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
+
+
+
 </script>
