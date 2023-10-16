38b1467147a6e67d1a1d1b4274ce89f2250c8462
Tufan <81257619+Tufan74@users.noreply.github.com>
2023-10-16 23:41:41 +0200

    new idea

--- M0.js
+++ M0.js
@@ -25,22 +25,3 @@ link2.addEventListener('click', () => {
 link3.addEventListener('click', () => {
     scrollToElement('.column');
 });
-
-
-
-
-var klickbareElemente = document.querySelectorAll('.klickbaresElement');
-
-klickbareElemente.forEach(function(element) {
-    element.addEventListener('click', function() {
-        // Code, der bei einem Klick ausgeführt wird
-        element.style.transition = 'transform 1s';
-        element.style.transform = 'translate(330px, 100px)'; // Neue Position
-
-        // Nach einer kurzen Verzögerung die Position zurücksetzen
-        setTimeout(function() {
-            element.style.transform = 'translate(0, 0)';
-        }, 1000); // Hier 1000 Millisekunden (1 Sekunde)
-    });
-});
-
